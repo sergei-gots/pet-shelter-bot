50fc3512066bbec7a5a91df0373cea35e97d12c8
ATsarev01 <maxperov01@gmail.com>
2023-06-24 18:29:48 +0600

    initial commit

---
+++ src/main/java/pro/sky/petshelterbot/PetShelterBotApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.petshelterbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetShelterBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetShelterBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/petshelterbot/PetShelterBotApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.petshelterbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetShelterBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
