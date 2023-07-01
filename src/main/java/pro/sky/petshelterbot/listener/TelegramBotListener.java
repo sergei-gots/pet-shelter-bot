bc402bcbefedf67004a7c261a8810948024288a8
Sergei-Gots <Sergei.Gots@yahoo.com>
2023-07-01 12:17:13 +0300

    Prepared to be merged with /dev

--- src/main/java/pro/sky/petshelterbot/listener/TelegramBotListener.java
+++ src/main/java/pro/sky/petshelterbot/listener/TelegramBotListener.java
@@ -2,12 +2,12 @@ package pro.sky.petshelterbot.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.CallbackQuery;
+import com.pengrad.telegrambot.model.Chat;
 import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -16,10 +16,14 @@ import java.util.List;
 @Service
 public class TelegramBotListener implements UpdatesListener {
 
-    private Logger logger = LoggerFactory.getLogger(TelegramBotListener.class);
+    final private Logger logger = LoggerFactory.getLogger(TelegramBotListener.class);
 
-    @Autowired
-    private TelegramBot telegramBot;
+
+    final private TelegramBot telegramBot;
+
+    public TelegramBotListener(TelegramBot telegramBot) {
+        this.telegramBot = telegramBot;
+    }
 
     @PostConstruct
     public void init() {
@@ -28,32 +32,53 @@ public class TelegramBotListener implements UpdatesListener {
 
     @Override
     public int process(List<Update> updates) {
-        updates.forEach(update -> {
-            logger.info("Processing update: {}", update);
-            if (update.message() != null) {
-                processMessage(update.message());
-            } else if (update.callbackQuery() != null) {
-                processCallbackQuery(update.callbackQuery());
-            }
-        });
+
+        logger.info("process(updates)-Method invoked");
+
+        try {
+            updates.forEach(update -> {
+                logger.info("- Processing update: {}", update);
+                if (update.message() != null) {
+                    processMessage(update.message());
+                } else if (update.callbackQuery() != null) {
+                    processCallbackQuery(update.callbackQuery());
+                }
+            });
+        } catch(Exception e) {
+            logger.error("process()-Method: Exception` e={} was caught", e, e);
+            e.printStackTrace();
+        }
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
 
     private void processMessage(Message message) {
-        if (message.text() != null && message.text().equals("/start")) {
-            String firstName = message.chat().firstName();
-            logger.info("Received /start command from user: " + firstName);
-            SendMessage welcomeMessage = new SendMessage(message.chat().id(), "Здравствуйте, " + firstName);
-            telegramBot.execute(welcomeMessage);
-        } else {
-            logger.info("Received message from user: " + message.text());
+        logger.info("processMessage({})-Method", message);
+
+        String text = message.text();
+        if(text == null) {
+            return;
         }
+        Chat chat = message.chat();
+        String firstName = chat.firstName();
+
+        if ("/start".equals(text)) {
+            logger.info("- Received /start command from user: " + firstName);
+            SendMessage welcomeMessage = new SendMessage(chat.id(), "Здравствуйте, " + firstName);
+            telegramBot.execute(welcomeMessage);
+            return;
+        }
+
+        logger.info("- There is no suitable handler for text=\"{}\" received from user={}",
+                firstName, text);
     }
 
     private void processCallbackQuery(CallbackQuery callbackQuery) {
-        if (callbackQuery.data().equals("start")) {
-            String firstName = callbackQuery.from().firstName();
-            SendMessage welcomeMessage = new SendMessage(callbackQuery.message().chat().id(), "Здравствуйте, " + firstName);
+        logger.info("processCallbackQuery({})-Method", callbackQuery);
+
+        if ("start".equals(callbackQuery.data())) {
+            SendMessage welcomeMessage = new SendMessage(
+                    callbackQuery.message().chat().id(),
+                    "Здравствуйте, " + callbackQuery.from().firstName());
             telegramBot.execute(welcomeMessage);
         }
     }
