231e0246d85c079ef6a0d73a9aad1fc14e72a39f
ATsarev01 <maxperov01@gmail.com>
2023-07-06 01:22:49 +0600

    Add ShelterInfoHandler and delete unusable method

--- src/main/java/pro/sky/petshelterbot/handler/ShelterInfoHandler.java
+++ src/main/java/pro/sky/petshelterbot/handler/ShelterInfoHandler.java
@@ -1,13 +1,9 @@
 package pro.sky.petshelterbot.handler;
 
 import com.pengrad.telegrambot.TelegramBot;
-import com.pengrad.telegrambot.model.CallbackQuery;
-import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.request.InlineKeyboardButton;
 import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
 import com.pengrad.telegrambot.request.SendMessage;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 /**
@@ -16,24 +12,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class ShelterInfoHandler {
 
-    final private Logger logger = LoggerFactory.getLogger(ShelterInfoHandler.class);
     final private TelegramBot telegramBot;
 
     public ShelterInfoHandler(TelegramBot telegramBot) {
         this.telegramBot = telegramBot;
     }
 
-    public void processCallbackQuery(CallbackQuery callbackQuery) {
-        logger.info("processCallbackQuery({})-Method", callbackQuery);
-
-        String data = callbackQuery.data();
-        Message message = callbackQuery.message();
-        long chatId = message.chat().id();
-
-        if (data.equals("shelter_info")) {
-            sendShelterInfo(Long.toString(chatId));
-        }
-    }
     public void sendShelterInfo(String chatId) {
         // Create buttons
         InlineKeyboardButton button1 = new InlineKeyboardButton("Расписание работы и адрес").callbackData("schedule_info");
