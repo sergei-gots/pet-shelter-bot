bc8c5a7b257bb45a6bb19bc23e11aad0c640654c
Sergei-Gots <Sergei.Gots@yahoo.com>
2023-07-27 20:23:42 +0300

    TelegramBotListenerTest initial commit

---
+++ src/test/java/pro/sky/petshelterbot/listener/TelegramBotListenerTest.java
@@ -0,0 +1,51 @@
+package pro.sky.petshelterbot.listener;
+
+import com.pengrad.telegrambot.BotUtils;
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.model.Update;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static pro.sky.petshelterbot.constants.Commands.START;
+
+@ExtendWith(MockitoExtension.class)
+class TelegramBotListenerTest {
+
+    @Mock
+    private TelegramBot telegramBot;
+
+    @InjectMocks
+    private TelegramBotListener telegramBotListener;
+
+    private Update getUpdate(String content) throws URISyntaxException, IOException {
+        String json = Files.readString(
+                Paths.get(TelegramBotListenerTest.class.getResource(
+                                "text_update.json"
+                        ).toURI()
+                )
+        );
+
+        return BotUtils.fromJson(
+                json.replace("%commdand%", content),
+                Update.class
+        );
+    }
+
+    @Test
+    void process() throws URISyntaxException, IOException {
+
+        telegramBotListener.process(
+                Collections.singletonList(
+                        getUpdate(START)));
+    }
+}
