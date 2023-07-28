24e6c28a7696247ae8a68407ee6c6b533c2fed65
Sergei-Gots <Sergei.Gots@yahoo.com>
2023-07-28 21:41:06 +0300

    TelegramBotListenerTest next step try: research in providing process(Updates) based on Hanler.handle(update) calls

--- src/test/java/pro/sky/petshelterbot/listener/TelegramBotListenerTest.java
+++ src/test/java/pro/sky/petshelterbot/listener/TelegramBotListenerTest.java
@@ -3,11 +3,19 @@ package pro.sky.petshelterbot.listener;
 import com.pengrad.telegrambot.BotUtils;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.request.ParseMode;
+import com.pengrad.telegrambot.request.SendMessage;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import pro.sky.petshelterbot.handler.*;
+import pro.sky.petshelterbot.repository.*;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -21,8 +29,42 @@ import static pro.sky.petshelterbot.constants.Commands.START;
 @ExtendWith(MockitoExtension.class)
 class TelegramBotListenerTest {
 
+    final private long  CHAT_ID = 123L;
+
     @Mock
     private TelegramBot telegramBot;
+    @Mock
+    private AdopterRepository adopterRepository;
+    @Mock
+    private VolunteerRepository volunteerRepository;
+    @Mock
+    private ShelterRepository shelterRepository;
+    @Mock
+    private UserMessageRepository userMessageRepository;
+    @Mock
+    private ButtonRepository buttonsRepository;
+    @Mock
+    private DialogRepository dialogRepository;
+    @SpyBean
+    private BasicAdopterHandler basicAdopterHandler =
+            new BasicAdopterHandler(telegramBot,
+                    adopterRepository,
+                    volunteerRepository,
+                    shelterRepository,
+                    userMessageRepository,
+                    buttonsRepository,
+                    dialogRepository );
+    @Mock
+    private VolunteerDialogHandler volunteerDialogHandler;
+    @Mock
+    private AdopterInputHandler adopterInputHandler;
+    @Mock
+    AdopterDialogHandler adopterDialogHandler;
+    @Mock
+    ShelterInfoHandler shelterInfoHandler;
+    @Mock
+    DefaultHandler defaultHandler;
+
 
     @InjectMocks
     private TelegramBotListener telegramBotListener;
@@ -47,5 +89,15 @@ class TelegramBotListenerTest {
         telegramBotListener.process(
                 Collections.singletonList(
                         getUpdate(START)));
+
+        ArgumentCaptor<SendMessage> argumentCaptor = ArgumentCaptor.forClass(SendMessage.class);
+        Mockito.verify(telegramBot).execute(argumentCaptor.capture());
+        SendMessage actual = argumentCaptor.getValue();
+
+        Assertions.assertThat(actual.getParameters().get("chat_id")).isEqualTo(CHAT_ID);
+        Assertions.assertThat(actual.getParameters().get("text")).isEqualTo(
+                "Для планирования задачи отправьте её в формате:\n*01.01.2022 20:00 Сделать домашнюю работу*");
+        Assertions.assertThat(actual.getParameters().get("parse_mode"))
+                .isEqualTo(ParseMode.Markdown.name());
     }
 }
